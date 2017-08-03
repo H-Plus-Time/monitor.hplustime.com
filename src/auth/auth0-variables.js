d6a863f4aff289c2404c9090ad8a7e6457302529
Nicholas Roberts <nicholas.roberts.au@gmail.com>
2017-08-04 02:23:42 +1000

    Perfected deployment

--- src/auth/auth0-variables.js
+++ src/auth/auth0-variables.js
@@ -1,5 +1,5 @@
 export const AUTH_CONFIG = {
-  clientId: process.env.CLIENT_ID,
-  domain: process.env.DOMAIN,
-  callbackUrl: 'http://monitor.hplustime.com/callback'
+  clientId: 'ILDYrsS9tUuMYYI5LEqurxrIN7I0s34V',
+  domain: 'hplustime.au.auth0.com',
+  callbackUrl: 'https://monitor.hplustime.com/callback'
 }
